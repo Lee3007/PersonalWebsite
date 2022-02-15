270c564a785740a6826d1ea971e069300ee08069
r3solv <felipe.lee30@gmail.com>
2022-02-15 15:42:01 -0300

    Added some links and tried out a way to scroll to a part of the page

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,18 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+
+function copyEmailToClipboard(){
+
+  navigator.clipboard.writeText("felipe.lee30@gmail.com");
+
+}
+
+// function scrollToHome(){
+
+//   document.getElementById('asdf').scrollIntoView({
+//     behavior: "smooth",
+//   });
+
+// }
